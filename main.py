c028e22b3f4a3e0ee808cf7072b40498019f2a47
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-12 10:03:50 +0200

    Serve spectra as JSON API

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from ascavis_data import alcdef, sha
+import httplib2
+import json
+from flask import Flask, Response
 
+
+API_MIME = "text/plain"
 
 app = Flask(__name__)
+spitzer = sha.SpitzerHeritageArchive(httplib2.Http(".cache"))
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return 'Hello World!'
 
 
-if __name__ == '__main__':
+@app.route("/spectrum/<int:jpl>")
+def spectrum(jpl):
+    observations = sha.parse_table(spitzer.query_by_jpl(253)[1])
+    spectrum = sha.parse_table(
+        spitzer.download_spectrum(filter(sha.is_spectrum, observations)[0])[1]
+    )
+    return Response(json.dumps(spectrum), mimetype=API_MIME)
+
+
+if __name__ == "__main__":
+    app.debug = True
     app.run()
