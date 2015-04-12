d6a4e648e19dced507e622d8a8efed016f909302
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-12 11:15:12 +0200

    Generalize spectrum output

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ spitzer = sha.SpitzerHeritageArchive(httplib2.Http(".cache"))
 def hello_world():
     return 'Hello World!'
 
-
 @app.route("/spectrum/<int:jpl>")
 def spectrum(jpl):
-    observations = sha.parse_table(spitzer.query_by_jpl(253)[1])
-    spectrum = sha.parse_table(
-        spitzer.download_spectrum(filter(sha.is_spectrum, observations)[0])[1]
+    observations = sha.parse_table(spitzer.query_by_jpl(jpl)[1])
+    spectra = map(
+        lambda obs: sha.parse_table(spitzer.download_spectrum(obs)[1]),
+        filter(sha.is_spectrum, observations)
     )
-    return Response(json.dumps(spectrum), mimetype=API_MIME)
+    return Response(json.dumps(spectra), mimetype=API_MIME)
 
 @app.route("/mpc/<int:jpl>")
 def mpc_call(jpl):
@@ -37,7 +37,6 @@ def mpc_call(jpl):
 @app.route('/app/<path:filename>')
 def send_file(filename):
     return send_from_directory(ASSET_DIR, filename)
-
 
 if __name__ == "__main__":
     app.debug = True
