adadbffab1627029d736d952516f868e07778d7f
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-12 11:01:20 +0200

    Serve static assets

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from ascavis_data import alcdef, sha
 import httplib2
 import json
-from flask import Flask, Response
+from flask import Flask, Response, send_from_directory
 
 
 API_MIME = "text/plain"
+ASSET_DIR = "assets/"
 
 app = Flask(__name__)
 spitzer = sha.SpitzerHeritageArchive(httplib2.Http(".cache"))
@@ -22,6 +23,11 @@ def spectrum(jpl):
         spitzer.download_spectrum(filter(sha.is_spectrum, observations)[0])[1]
     )
     return Response(json.dumps(spectrum), mimetype=API_MIME)
+
+
+@app.route('/app/<path:filename>')
+def send_file(filename):
+    return send_from_directory(ASSET_DIR, filename)
 
 
 if __name__ == "__main__":
